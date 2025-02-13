4e2a9b56810fd8e5ca5b07f0593151d298de0a55
topaz1208 <topaz1208@gmail.com>
2025-02-13 19:41:13 +0900

    first commit

---
+++ practice/app.js
@@ -0,0 +1,33 @@
+let text = 'JavaScriptの練習';
+console.log(text);
+
+text = 'JavaScriptをマスターした';
+console.log(text);
+
+let longText =
+ 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+ console.log(longText);
+ console.log(longText);
+ console.log(longText);
+ // 処理②
+ console.log(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  );
+  console.log(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  );
+  console.log(
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  );
+
+let testText;
+console.log(testText);
+
+let name = 'ジョニー'
+let greet = '私は' + name + '!'
+console.log(greet);
+
+let template = 'テンプレートリテラル';
+let templateText = `これが${template}です。
+${template}は改行もできます`
+console.log(templateText);
