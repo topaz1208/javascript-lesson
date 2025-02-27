90bdab6bd4069910c2429b4b3db97a46637b6c09
淺海莉絵 <asakairie@asakairienoMacBook-Air.local>
2025-02-27 21:08:07 +0900

    宿題追加

--- question/app.js
+++ question/app.js
@@ -36,7 +36,7 @@ let playerList = [
       favorites: ['Football', 'Smash Bros.'],
     }
   ];
-  console.log(playerList[1].favorites[1])
+  console.log(playerList[2].favorites[0])
   
 //Q5
 console.log((playerList[0].age+playerList[1].age+playerList[2].age)/3)
@@ -89,18 +89,28 @@ calc.multiply(7, 7)
 calc.divide(25, 5)
 
 //Q9
+// function remainder(x, y){
+//    let result = x % y;
+//    console.log( x + 'を' + y + 'で割った余りは' + result + 'です');
+//    return result ; 
+//  }
+//  remainder(5, 3)
+
+//⭕️宿題 返り値を使って問題文通りにやる方法
 function remainder(x, y){
-  let result = x % y;
-  console.log( x + 'を' + y + 'で割った余りは' + result + 'です');
-  return x % y ; 
-}
-remainder(5, 3)
+  return  x % y ; 
+ }  
+ let p = 5 ;
+ let o = 3 ;
+ let result = remainder(p, o) ;
+ console.log( p + 'を' + o + 'で割った余りは' + result + 'です');
+
 
 //Q10
-function foo() {
-let x = 1;
-}
-console.log(x);
+// function foo() {
+// let x = 1;
+// }
+// console.log(x);
 //変数xはスコープが関数内のため、関数外にあるconsoleでは変数xが参照できずにエラーが出力される。
 //関数外で出力を行いたい場合は、変数xの定義を関数外で行い、関数内で変数xの値を再代入するようにする。
 
@@ -110,10 +120,16 @@ var random = Math.floor(Math.random() * 10)
 console.log(random );
 
 //Q2 
-function HelloWorld(){
-    console.log('Hello World!');
-};
-setTimeout(HelloWorld, 3000)
+// function HelloWorld(){
+//     console.log('Hello World!');
+// };
+// setTimeout(HelloWorld, 3000)
+
+//宿題 一個でかける
+setTimeout(function HelloWorld(){
+  console.log('Hello World!');
+}, 3000);
+
 
 //Q3
 let num = 0 ;
@@ -126,9 +142,16 @@ console.log('num is 0');
 }
 
 //Q4 
+// let numbers = [];
+// for (let i = 0; i < 100; i++ ){
+//   numbers[numbers.length] = i;
+// };
+// console.log( numbers );
+
+//宿題 pushを使う
 let numbers = [];
 for (let i = 0; i < 100; i++ ){
-  numbers[numbers.length] = i;
+  numbers.push(i);
 };
 console.log( numbers );
 
@@ -139,5 +162,5 @@ for (let i = 0; i < mixed.length; i++){
     console.log(mixed[i] % 2 === 0 ? 'even' : 'odd');
   }else{
     console.log('not number');
-  }  
+ }  
 }; 
